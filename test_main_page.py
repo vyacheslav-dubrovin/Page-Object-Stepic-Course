2c355d8fb8f2d229efcbb82f04977d386184d9d8
dubrovin.v <dubrovin.v@bstd.ru>
2023-11-29 09:31:04 +0700

    Add jump to login page

--- test_main_page.py
+++ test_main_page.py
@@ -5,8 +5,10 @@ from pages.login_page import LoginPage
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
-    page.open(browser, link)
+    page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
